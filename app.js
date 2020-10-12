59aa5789f625971ec33e60d778d61769d6903ea1
MLT-NYC <maxtrukhin@gmail.com>
2020-10-11 22:43:50 -0400

    Added express for routing

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+const users = require('./routes/api/users');
+const messages = require('./routes/api/messages');
 
-app.get('/', (req, res) => res.send('Yo World'));
+app.get('/', (req, res) => res.send('To World'));
+app.use('/api/users', users);
+app.use('/api/messages', messages);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 mongoose
