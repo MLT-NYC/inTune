bead3f27b6d5a418b154df4cd0f1d3c67b8fc3a8
MLT-NYC <maxtrukhin@gmail.com>
2020-10-11 20:53:07 -0400

    Updated for deprecation

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 mongoose
-    .connect(db, { useNewUrlParser: true })
+    .connect(db, { useNewUrlParser: true , useUnifiedTopology: true}  )
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
